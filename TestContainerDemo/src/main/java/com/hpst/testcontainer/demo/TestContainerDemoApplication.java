d33117c5be4b562ee4fefdf5b20ad7247c0ad344
harpal singh <harpal.rtu@gmail.com>
2020-09-24 07:55:36 +0530

    initial check in for test-containers/mysql in docker container running

---
+++ TestContainerDemo/src/main/java/com/hpst/testcontainer/demo/TestContainerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hpst.testcontainer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainerDemoApplication.class, args);
+	}
+
+}
---
+++ TestContainerDemo/src/test/java/com/hpst/testcontainer/demo/TestMySqlTestContainer.java
@@ -0,0 +1,38 @@
+package com.hpst.testcontainer.demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+/**
+ * @author harpal
+ *
+ */
+@Testcontainers
+@TestInstance(Lifecycle.PER_CLASS)
+public class TestMySqlTestContainer {
+
+    // will be shared between test methods
+    @Container
+    private static final MySQLContainer<?> MY_SQL_CONTAINER = new MySQLContainer<>();
+
+
+   @BeforeAll
+   public void startMySqlContainer() {
+		MY_SQL_CONTAINER.start();
+   }
+    
+    @Test
+    @DisplayName("isMsqlContainerRunning")
+    void isMsqlContainerRunning() {
+    	assertTrue(MY_SQL_CONTAINER.isRunning());
+    }
+}
